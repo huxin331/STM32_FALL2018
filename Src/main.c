7a4edfcd9cc16757bba595a2f457f4351efba63a
Hanwen Zhao <zhaolhw@gmail.com>
2018-11-27 16:55:21 -0500

    triple interleaving working code
    Only start once

--- Src/main.c
+++ Src/main.c
@@ -49,55 +49,52 @@
 
 /* Private variables ---------------------------------------------------------*/
 ADC_HandleTypeDef hadc1;
+ADC_HandleTypeDef hadc2;
+ADC_HandleTypeDef hadc3;
 DMA_HandleTypeDef hdma_adc1;
 
 UART_HandleTypeDef huart1;
 
 /* USER CODE BEGIN PV */
 /* Private variables ---------------------------------------------------------*/
-#define NUM_SAMPLES 250  // number of samples per scanline
-__IO uint8_t data[NUM_SAMPLES+2];  // TEMPORARY HACK: the extra 2 slots at the end are for copying the rel_angle value before sending the Ethernet packet
-uint16_t value[1000];
-float numSample = 1000;
-
-uint32_t counter = 0;
-__IO uint32_t read_value = 0;
-int startCounter = 0;
-int endCounter = 0;
+#define ADC_BUFFER_LENGTH 10000
+__IO uint8_t ADCBuffer[ADC_BUFFER_LENGTH];
+int startCounter;
+int endCounter;
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
-//static void MX_DMA_Init(void);
-static void MX_USART1_UART_Init(void);
+static void MX_DMA_Init(void);
 static void MX_ADC1_Init(void);
+static void MX_ADC2_Init(void);
+static void MX_ADC3_Init(void);
+static void MX_USART1_UART_Init(void);
 
 /* USER CODE BEGIN PFP */
 /* Private function prototypes -----------------------------------------------*/
 void vprint(const char *fmt, va_list argp);
 void my_printf(const char *fmt, ...);
-
+void HAL_ADC_ConvCpltCallback(ADC_HandleTypeDef* AdcHandle)
+{
+	//HAL_GPIO_TogglePin(GPIOB,GPIO_PIN_0|GPIO_PIN_14|GPIO_PIN_7);
+	//HAL_ADCEx_MultiModeStop_DMA(&hadc1);
+    //HAL_ADC_Stop(&hadc3);   // Although it doesn't matter
+    //HAL_ADC_Stop(&hadc2);   // since prev function returns error
+    my_printf("ADC Stop!\r\n");
+    endCounter = HAL_GetTick();
+    float ADC_Speed = ADC_BUFFER_LENGTH/(float)((float)(endCounter - startCounter)/1000);
+    //my_printf("Start Counter: %d \r\n", startCounter);
+    //my_printf("End Counter: %d \r\n", endCounter);
+    my_printf("ADC Speed: %f samples/s \r\n", ADC_Speed);
+    for (int i = 0; i < 1000; i++){
+    	my_printf("%d \r\n", ADCBuffer[i]);
+    }
+}
 /* USER CODE END PFP */
 
 /* USER CODE BEGIN 0 */
-
-/*
-void HAL_ADC_ConvCpltCallback(ADC_HandleTypeDef* hadc){
-	value[0] = read_value;
-	//my_printf("Counter: %d \r\n", counter);
-	//my_printf("ADC Reading: %d \r\n", read_value);
-	counter ++;
-	if (counter >= 10000){
-		endCounter = HAL_GetTick();
-		my_printf("End Time: %d \r\n", endCounter);
-		my_printf("Stop DMA \r\n");
-		HAL_ADC_Stop_DMA(&hadc1);
-		HAL_ADC_Stop_DMA(&hadc1);
-	}
-}
-*/
-
 
 /* USER CODE END 0 */
 
@@ -126,18 +123,18 @@ int main(void)
 
   /* USER CODE BEGIN SysInit */
 
-
   /* USER CODE END SysInit */
 
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
-  //MX_DMA_Init();
-  MX_USART1_UART_Init();
+  MX_DMA_Init();
   MX_ADC1_Init();
-  HAL_ADC_Start(&hadc1);
+  MX_ADC2_Init();
+  MX_ADC3_Init();
+  MX_USART1_UART_Init();
   /* USER CODE BEGIN 2 */
-
-
+  HAL_ADC_Start(&hadc3);
+  HAL_ADC_Start(&hadc2);
   /* USER CODE END 2 */
 
   /* Infinite loop */
@@ -146,48 +143,18 @@ int main(void)
   {
 
   /* USER CODE END WHILE */
+	  //HAL_GPIO_TogglePin(GPIOB,GPIO_PIN_0|GPIO_PIN_14|GPIO_PIN_7);
+	  //my_printf("Start Triple Interleaving!\r\n");
+	  //HAL_Delay(1);
+
+	  //startCounter = HAL_GetTick();
+
+      HAL_ADCEx_MultiModeStart_DMA(&hadc1, (uint32_t*) &ADCBuffer, ADC_BUFFER_LENGTH);
+
+
+
 
   /* USER CODE BEGIN 3 */
-
-	  //HAL_GPIO_TogglePin(GPIOB,GPIO_PIN_0|GPIO_PIN_14|GPIO_PIN_7);
-	  //char *msg = "Hello\r\n";
-	  //my_printf(msg);
-
-	  HAL_ADC_PollForConversion(&hadc1, 10);
-	  uint8_t value = HAL_ADC_GetValue(&hadc1);
-	  my_printf("ADC: %d \r\n",value);
-	  /*
-	  startCounter = HAL_GetTick();
-	  for (int i = 0; i < numSample; i++){
-		  value[i] = HAL_ADC_GetValue(&hadc1);
-	  }
-	  endCounter = HAL_GetTick();
-	  */
-
-	  //my_printf("ADC Reading: %d\r\n", value);
-	  /*
-	  for (int i = 0; i < 1000; i++){
-		  my_printf("%d\r\n",value[i]);
-	  }
-	  */
-
-	  //float ADC_Speed = numSample/(float)((float)(endCounter - startCounter)/1000);
-	  //my_printf("ADC Speed without DMA: %f samples/s \r\n", ADC_Speed);
-
-	  //my_printf("Start Time: %d \r\n", startCounter);
-	  //my_printf("End Time: %d \r\n", endCounter);
-	  //my_printf("ADC Speed without DMA: %f samples/s \r\n", ADC_Speed);
-
-	  //my_printf("Hello\r\n");
-	  // DMA
-	  /*
-	  startCounter = HAL_GetTick();
-	  my_printf("Start Time: %d \r\n", startCounter);
-	  HAL_ADC_Start_DMA(&hadc1, (uint32_t*)&read_value, 1);
-	  float ADC_Speed = 100000/(float)((float)(endCounter - startCounter)/1000);
-	  my_printf("ADC Speed with DMA: %f samples/s \r\n", ADC_Speed);
-	  */
-
 
   }
   /* USER CODE END 3 */
@@ -263,6 +230,7 @@ void SystemClock_Config(void)
 static void MX_ADC1_Init(void)
 {
 
+  ADC_MultiModeTypeDef multimode;
   ADC_ChannelConfTypeDef sConfig;
 
     /**Configure the global features of the ADC (Clock, Resolution, Data Alignment and number of conversion) 
@@ -278,8 +246,18 @@ static void MX_ADC1_Init(void)
   hadc1.Init.DataAlign = ADC_DATAALIGN_RIGHT;
   hadc1.Init.NbrOfConversion = 1;
   hadc1.Init.DMAContinuousRequests = DISABLE;
-  hadc1.Init.EOCSelection = DISABLE;
+  hadc1.Init.EOCSelection = ADC_EOC_SINGLE_CONV;
   if (HAL_ADC_Init(&hadc1) != HAL_OK)
+  {
+    _Error_Handler(__FILE__, __LINE__);
+  }
+
+    /**Configure the ADC multi-mode 
+    */
+  multimode.Mode = ADC_TRIPLEMODE_INTERL;
+  multimode.DMAAccessMode = ADC_DMAACCESSMODE_3;
+  multimode.TwoSamplingDelay = ADC_TWOSAMPLINGDELAY_5CYCLES;
+  if (HAL_ADCEx_MultiModeConfigChannel(&hadc1, &multimode) != HAL_OK)
   {
     _Error_Handler(__FILE__, __LINE__);
   }
@@ -290,6 +268,76 @@ static void MX_ADC1_Init(void)
   sConfig.Rank = ADC_REGULAR_RANK_1;
   sConfig.SamplingTime = ADC_SAMPLETIME_3CYCLES;
   if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
+  {
+    _Error_Handler(__FILE__, __LINE__);
+  }
+
+}
+
+/* ADC2 init function */
+static void MX_ADC2_Init(void)
+{
+
+  ADC_ChannelConfTypeDef sConfig;
+
+    /**Configure the global features of the ADC (Clock, Resolution, Data Alignment and number of conversion) 
+    */
+  hadc2.Instance = ADC2;
+  hadc2.Init.ClockPrescaler = ADC_CLOCK_SYNC_PCLK_DIV2;
+  hadc2.Init.Resolution = ADC_RESOLUTION_8B;
+  hadc2.Init.ScanConvMode = DISABLE;
+  hadc2.Init.ContinuousConvMode = ENABLE;
+  hadc2.Init.DiscontinuousConvMode = DISABLE;
+  hadc2.Init.DataAlign = ADC_DATAALIGN_RIGHT;
+  hadc2.Init.NbrOfConversion = 1;
+  hadc2.Init.DMAContinuousRequests = DISABLE;
+  hadc2.Init.EOCSelection = DISABLE;
+  if (HAL_ADC_Init(&hadc2) != HAL_OK)
+  {
+    _Error_Handler(__FILE__, __LINE__);
+  }
+
+    /**Configure for the selected ADC regular channel its corresponding rank in the sequencer and its sample time. 
+    */
+  sConfig.Channel = ADC_CHANNEL_0;
+  sConfig.Rank = ADC_REGULAR_RANK_1;
+  sConfig.SamplingTime = ADC_SAMPLETIME_3CYCLES;
+  if (HAL_ADC_ConfigChannel(&hadc2, &sConfig) != HAL_OK)
+  {
+    _Error_Handler(__FILE__, __LINE__);
+  }
+
+}
+
+/* ADC3 init function */
+static void MX_ADC3_Init(void)
+{
+
+  ADC_ChannelConfTypeDef sConfig;
+
+    /**Configure the global features of the ADC (Clock, Resolution, Data Alignment and number of conversion) 
+    */
+  hadc3.Instance = ADC3;
+  hadc3.Init.ClockPrescaler = ADC_CLOCK_SYNC_PCLK_DIV2;
+  hadc3.Init.Resolution = ADC_RESOLUTION_8B;
+  hadc3.Init.ScanConvMode = DISABLE;
+  hadc3.Init.ContinuousConvMode = ENABLE;
+  hadc3.Init.DiscontinuousConvMode = DISABLE;
+  hadc3.Init.DataAlign = ADC_DATAALIGN_RIGHT;
+  hadc3.Init.NbrOfConversion = 1;
+  hadc3.Init.DMAContinuousRequests = DISABLE;
+  hadc3.Init.EOCSelection = DISABLE;
+  if (HAL_ADC_Init(&hadc3) != HAL_OK)
+  {
+    _Error_Handler(__FILE__, __LINE__);
+  }
+
+    /**Configure for the selected ADC regular channel its corresponding rank in the sequencer and its sample time. 
+    */
+  sConfig.Channel = ADC_CHANNEL_0;
+  sConfig.Rank = ADC_REGULAR_RANK_1;
+  sConfig.SamplingTime = ADC_SAMPLETIME_3CYCLES;
+  if (HAL_ADC_ConfigChannel(&hadc3, &sConfig) != HAL_OK)
   {
     _Error_Handler(__FILE__, __LINE__);
   }
@@ -320,8 +368,17 @@ static void MX_USART1_UART_Init(void)
 /** 
   * Enable DMA controller clock
   */
+static void MX_DMA_Init(void) 
+{
+  /* DMA controller clock enable */
+  __HAL_RCC_DMA2_CLK_ENABLE();
 
+  /* DMA interrupt init */
+  /* DMA2_Stream0_IRQn interrupt configuration */
+  HAL_NVIC_SetPriority(DMA2_Stream0_IRQn, 0, 0);
+  HAL_NVIC_EnableIRQ(DMA2_Stream0_IRQn);
 
+}
 
 /** Configure pins as 
         * Analog 
@@ -341,10 +398,10 @@ static void MX_GPIO_Init(void)
   __HAL_RCC_GPIOB_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_0|GPIO_PIN_14|GPIO_PIN_15|GPIO_PIN_7, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_0|GPIO_PIN_14|GPIO_PIN_7, GPIO_PIN_RESET);
 
-  /*Configure GPIO pins : PB0 PB14 PB15 PB7 */
-  GPIO_InitStruct.Pin = GPIO_PIN_0|GPIO_PIN_14|GPIO_PIN_15|GPIO_PIN_7;
+  /*Configure GPIO pins : PB0 PB14 PB7 */
+  GPIO_InitStruct.Pin = GPIO_PIN_0|GPIO_PIN_14|GPIO_PIN_7;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
@@ -353,8 +410,6 @@ static void MX_GPIO_Init(void)
 }
 
 /* USER CODE BEGIN 4 */
-
-
 void vprint(const char *fmt, va_list argp)
 {
     char string[200];
@@ -386,6 +441,7 @@ void _Error_Handler(char *file, int line)
   /* User can add his own implementation to report the HAL error return state */
   while(1)
   {
+	  my_printf("Error!");
   }
   /* USER CODE END Error_Handler_Debug */
 }
@@ -416,3 +472,4 @@ void assert_failed(uint8_t* file, uint32_t line)
   */
 
 /************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
+
